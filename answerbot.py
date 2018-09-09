36dd9fbf03c077a003f3938c6023d0b6fd4f64bc
makurell <makureoll@gmail.com>
2018-09-09 15:50:06 +0100

    impl logging

--- answerbot.py
+++ answerbot.py
@@ -2,10 +2,21 @@ from python_log_indenter import IndentedLoggerAdapter
 import logging
 import spacy
 nlp=spacy.load('en')
-log=IndentedLoggerAdapter(logging.getLogger(__name__))
-logging.basicConfig(level=logging.DEBUG,format='[%(asctime)s][%(levelname)s] %(message)s',datefmt='%H:%M')
 
 class AnswerBot:
+    def __init__(self,debug=True):
+        #logging setup
+        logger=logging.getLogger(__name__)
+
+        formatter=logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s', datefmt='%H:%M')
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+        self.log=IndentedLoggerAdapter(logger)
+        self.log.setLevel(logging.DEBUG if debug else logging.WARNING)
+
     @staticmethod
     def fix_question(text:str):
         if text.endswith('.'):
@@ -20,28 +31,51 @@ class AnswerBot:
         :return: queries:[parts:[token]]
         """
         doc=nlp(self.fix_question(text))
-        log.debug(str(doc.print_tree()))
-        log.info("parsing question: "+str(doc))
+        self.log.debug(str(doc.print_tree()))
+        self.log.info("parsing question: "+str(doc))
+        self.log.add()
 
         ret=[]
         for sent in doc.sents:
             ret.extend(self.parse_sent(sent))
+        self.log.sub()
+        self.log.info(str(ret))
         return ret
 
     def parse_sent(self, sent):
         #todo implement more query splitting here (e.g 'and')
+        self.log.info("parsing sent: "+str(sent))
         return [self.parse_span(sent)]
 
     def parse_span(self,span):
-        return self.parse_children(span.root)
+        self.log.info("parsing span: "+str(span))
+        self.log.add()
+        ret = self.parse_children(span.root)
+        self.log.sub()
+        self.log.info("<<"+str(ret))
+        return ret
 
     def parse_children(self,root):
+        self.log.info("parsing: "+str(root))
+        self.log.add()
+
         ret=[]
         for child in root.children:
             if child.dep_=='poss':
-                ret.append(root)
+                ret.append(child)
                 ret.extend(self.parse_children(child))
+                ret.append(root)
+            elif child.dep_=='case' or 'punct':
+                pass
+            else:
+                ret.extend(self.parse_children(child))
+
+        if len(ret)==0 and len(list(root.children))==0:
+            ret.append(root)
+
+        self.log.sub()
+        self.log.info("<<"+str(ret))
         return ret
 
 if __name__=='__main__':
-    log.info(str(AnswerBot().parse_question("Obama's age")))
+    AnswerBot().parse_question("Obama's age")
--- tests.py
+++ tests.py
@@ -1,14 +1,22 @@
 import unittest
 from answerbot import AnswerBot
 
+def strip_parsed(parsed):
+    ret=[]
+    for query in parsed:
+        buf=[]
+        for token in query:
+            buf.append(str(token))
+        ret.append(buf)
+    return ret
+
 class QuestionParsing(unittest.TestCase):
+    def assertParsed(self, bot, query, other):
+        self.assertEqual(strip_parsed(bot.parse_question(query)), other)
+
     def test_a_basic(self):
-        bot=AnswerBot()
-        self.assertEqual(bot.parse_question("Obama's age"),
-                         [
-                             [('Obama',1),
-                              ('age',0)]
-                         ])
+        bot=AnswerBot(debug=False)
+        self.assertParsed(bot,"Obama's age",[['Obama','age']])
 
 if __name__=='__main__':
     unittest.main(verbosity=2)
